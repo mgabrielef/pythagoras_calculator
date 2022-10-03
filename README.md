8e2318d6726aa651e4cfb5b57b3344a08a9bf53a
Gabriele Ferreira <mariagabrieleoliveira@gmail.com>
2022-10-03 19:49:26 -0300

    alteracoes

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# pythagoras_calculator
+# PYTHAGORAS_CALCULATOR
+
+Projeto para descobrir os valores dos catetos e hipotenusa de acordo com o teorema de pitágoras utilizando Flask.
+
+Tecnologias utilizadas:
+- Python
+- Flask
+
+## ÍNDICE
+* [Utilizando o projeto](#Utilizando-o-projeto)
+* [Inicialização](#Inicialização)
+* [Métodos do projeto](#Métodos-do-projeto)
+* [Utilizando](#Utilizando)
+
+## Utilizando o projeto
+
+Para utilizar o projeto execute:
+
+```
+git clone https://github.com/mgabrielef/pythagoras_calculator
+```
+
+## Inicialização
+
+Para iniciar o projeto será necessário ter o Flask instalado em sua máquina:
+
+```
+$ pip install -U Flask
+```
+
+# Métodos do projeto
+
+Calcular hipotenusa
+
+```python
+  
+```  
+
+Calcular cateto oposto
+
+```
+  
+```
+Calcular adjacente
+
+```
+  
+```
+
+# Utilizando
+
+Para rodar o código:
+```
+$ python run app.py
+```
+
+
+Você pode acessar também o [HEROKU APP](https://mysterious-mesa-79161.herokuapp.com)
--- app.py
+++ app.py
@@ -41,29 +41,6 @@ def calculaHipotenusa():
     return render_template("resultadoHipotenusa.html", value=msg, info=msg2)    
 
 
-#Definindo rota para página "CALCULAR CATETO ADJACENTE"     
-@app.route("/catetoAdj", methods=["GET"])
-def pagCatetoAdj():
-  return render_template("catetoAdj.html")
-
-
-#Criando função para calcular o valor do Cateto Adjacente
-@app.route("/resultadoCatetoAdj", methods=["POST", "GET"])
-def calculaCatetoAdj():
-  #Requisitando valores inseridos no form pela name tag
-  catetoX = int(request.form['catetoX'])
-  hipotenusaX = int(request.form['hipotenusaX'])
-  #Realizando cálculo para obter valor do cateto adjacente
-  catetoY = math.sqrt((hipotenusaX**2) - (catetoX**2))
-  #Inserindo condição, se o valor do cateto for maior que 0 irá redirecionar a página e retornar o valor
-  if catetoY > 0:
-    return render_template("resultadoCatetoAdj.html", value = catetoY)
-  #Se o valor do cateto não condizir com a condição anterior irá redirecionar a página e exibir mensagem
-  else:
-    msg = "Número inválido\nInsira novos números"
-    return render_template("resultadoCatetoAdj.html", value=msg)
-
-
 #Definindo rota para página "CALCULAR CATETO OPOSTO" 
 @app.route("/catetoOp", methods=["GET"])
 def pagCatetoOp():
@@ -86,6 +63,28 @@ def calculaCatetoOp():
     msg = "Número inválido\nInsira novos números"
     return render_template("resultadoCatetoOp.html", value=msg)
 
+
+#Definindo rota para página "CALCULAR CATETO ADJACENTE"     
+@app.route("/catetoAdj", methods=["GET"])
+def pagCatetoAdj():
+  return render_template("catetoAdj.html")
+
+
+#Criando função para calcular o valor do Cateto Adjacente
+@app.route("/resultadoCatetoAdj", methods=["POST", "GET"])
+def calculaCatetoAdj():
+  #Requisitando valores inseridos no form pela name tag
+  catetoX = int(request.form['catetoX'])
+  hipotenusaX = int(request.form['hipotenusaX'])
+  #Realizando cálculo para obter valor do cateto adjacente
+  catetoY = math.sqrt((hipotenusaX**2) - (catetoX**2))
+  #Inserindo condição, se o valor do cateto for maior que 0 irá redirecionar a página e retornar o valor
+  if catetoY > 0:
+    return render_template("resultadoCatetoAdj.html", value = catetoY)
+  #Se o valor do cateto não condizir com a condição anterior irá redirecionar a página e exibir mensagem
+  else:
+    msg = "Número inválido\nInsira novos números"
+    return render_template("resultadoCatetoAdj.html", value=msg)
 
 #Certificando se o app já está aberto, se não, irá abrir
 if __name__ == '__main__':
